b8fe817e9f674bb1bdf07d0020e3bc3caf336c77
joeljhou <joeljhou336@gmail.com>
2021-08-27 14:32:24 +0800

    [88]合并两个有序数组  直接合并后排序&双指针

--- src/leetcode/editor/cn/doc/src/Solution.java
+++ src/leetcode/editor/cn/doc/src/Solution.java
@@ -1,22 +1,40 @@
-import java.util.Arrays;
-import java.util.HashMap;
-
 /**
  * 解决方案
  */
 public class Solution {
 
     /**
-     * 哈希表
+     * 官方推荐方法二：双指针
      */
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (hashMap.containsKey(target-nums[i])){
-                return new int[]{hashMap.get(target-nums[i]),i};
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = 0, p2 = 0;
+        int[] sorted = new int[m + n];
+        int cur;
+        while (p1 < m || p2 < n) {
+            if (p1 == m) {
+                cur = nums2[p2++];
+            } else if (p2 == n) {
+                cur = nums1[p1++];
+            } else if (nums1[p1] < nums2[p2]) {
+                cur = nums1[p1++];
+            } else {
+                cur = nums2[p2++];
             }
-            hashMap.put(nums[i],i);
+            sorted[p1 + p2 - 1] = cur;
         }
-        return new int[0];
+
+        for (int i = 0; i != m + n; i++) {
+            nums1[i] = sorted[i];
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        //[1,2,3,0,0,0]
+        //3
+        //[2,5,6]
+        //3
+        solution.merge(new int[]{1, 2, 2, 3, 0, 0, 0}, 4, new int[]{2, 5, 8}, 3);
     }
 }
