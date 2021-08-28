4d8e901cdd83371afdf1cb08a04e3b932e84e0e1
joeljhou <joeljhou336@gmail.com>
2021-08-28 15:00:25 +0800

    算法    [704]二分查找

---
+++ src/leetcode/editor/cn/[704]二分查找.java
@@ -0,0 +1,52 @@
+//给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否
+//则返回 -1。 
+//
+// 
+//示例 1: 
+//
+// 输入: nums = [-1,0,3,5,9,12], target = 9
+//输出: 4
+//解释: 9 出现在 nums 中并且下标为 4
+// 
+//
+// 示例 2: 
+//
+// 输入: nums = [-1,0,3,5,9,12], target = 2
+//输出: -1
+//解释: 2 不存在 nums 中因此返回 -1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 你可以假设 nums 中的所有元素是不重复的。 
+// n 将在 [1, 10000]之间。 
+// nums 的每个元素都将在 [-9999, 9999]之间。 
+// 
+// Related Topics 数组 二分查找 👍 330 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+
+    /**
+     * 官方推荐方法一：二分查找
+     * 解答成功: 执行耗时:0 ms,击败了100.00% 的Java用户 内存消耗:39.5 MB,击败了19.81% 的Java用户
+     */
+    public int search(int[] nums, int target) {
+        int pivot, left = 0, right = nums.length - 1;
+        while (left <= right) {
+            pivot = left + (right - left) / 2;
+            if (nums[pivot] == target) return pivot;
+            if (target < nums[pivot]) right = pivot - 1;
+            else left = pivot + 1;
+        }
+        return -1;
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
