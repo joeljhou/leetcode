b2033bf9d35e1f8ac3c9300492b2e220eb2fb9c8
joeljhou <joeljhou336@gmail.com>
2021-08-28 15:48:57 +0800

    [278]第一个错误的版本    二分查找

--- src/leetcode/editor/cn/doc/src/Solution.java
+++ src/leetcode/editor/cn/doc/src/Solution.java
@@ -6,25 +6,15 @@ import java.util.List;
  */
 public class Solution {
 
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> ret = new ArrayList<>();
-        for (int i = 0; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>();
-            for (int j = 0; j < i + 1; j++) {
-                if (i == 0 || j==0 || i == j) {
-                    row.add(1);
-                } else {
-                    row.add(ret.get(i-1).get(j - 1) + ret.get(i-1).get(j));
-                }
-            }
-            ret.add(row);
-        }
-        return ret;
+
+    public int firstBadVersion(int n) {
+
+        return 1;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.generate(5));
+        System.out.println(1 / 2);
     }
 
 }
